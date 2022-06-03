d5e6ea3cfa19dfdcfc57490aca610f2f83b955b4
Vinaya001 <93110662+vg667@users.noreply.github.com>
2022-06-03 04:58:00 +0000

    done

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,6 @@
 # Variables, Expressions & Statements
 
 hrs = float(input("Enter hours? "))
+rate=float(input("enter the rate per hrs"))
+pay=hrs*rate
+print("total pay=",pay)
--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,10 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = float(input("enter the hrs"))
+pay=float(input("enter the pay"))
+if(hrs>40);
+overtime=(hrs-40)*(pay*0.5)
+tq=(hrs*pay)+overtime
+else:
+tq=hrs*pay
+print("total pay=",tq)
